4a09c042923001eaa139a9b9740cf038ff9623d4
Michael Christenson II <mchristenson@revspringinc.com>
2013-08-26 16:54:21 -0700

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialdo::Application.config.secret_key_base = 'b31683ed0e9ba9acd0ac7bd8bbd5329c4fae7b973f0100ec033654379a3b7ee1bd65c36efba663403f9b7d5d54344cfe32173d6ec90c31a30af1584d44b96369'
